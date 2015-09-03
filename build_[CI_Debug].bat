44b7b861ca84d7a83f5c1d5d2d0c0a3ba89b8326
reg <entry.reg@gmail.com>
2015-09-03 17:25:21 +0300

    Updated NLog v4.1.0 & vsSBE.CI.MSBuild v1.5.0

--- build_[CI_Debug].bat
+++ build_[CI_Debug].bat
@@ -1,2 +1,2 @@
 .nuget\nuget restore vsSolutionBuildEvent_net40.sln 
-"C:\Program Files (x86)\MSBuild\12.0\bin\msbuild.exe" "vsSolutionBuildEvent_net40.sln" /verbosity:detailed  /l:"packages\vsSBE.CI.MSBuild.1.4.0\bin\CI.MSBuild.dll" /m:12 /t:Rebuild /p:Configuration=CI_Debug /p:Platform="Any CPU"
+"C:\Program Files (x86)\MSBuild\12.0\bin\msbuild.exe" "vsSolutionBuildEvent_net40.sln" /verbosity:detailed  /l:"packages\vsSBE.CI.MSBuild.1.5.0\bin\CI.MSBuild.dll" /m:12 /t:Rebuild /p:Configuration=CI_Debug /p:Platform="Any CPU"
