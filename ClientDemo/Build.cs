3F/vsSolutionBuildEvent

commit 26dba549d1e875314a80a691ae21576c1bb753c6

Major compatible changes of configuration.
* Added user configuration - .vssbe.user
* * Value of zooming & Word wrapping of main editor for each Event/Action type.
* * DebugMode & CacheData of binaries for C# Mode

* The LastTime property from C# Mode is obsolete and will be removed soon.
* * Added CacheData instead, with remote value from .vssbe.user - used IManager for work with CacheHeader.

* Updated Configuration.Header
* Added GlobalConfig: based on UserConfig(currently at least). In general, this useful for all priority settings before initialization of our engines. Default path - ApplicationData + APP_NAME
* +IAppSettings & new non static Settings. It also provides DebugModeUpdated event - Useful for clients, I remember... for example with IEntryPointClient.
* SolutionEvents is now specified with ISolutionEvents
* New Command__ property as wrapper of original Command in .vssbe settings. For convenient using directly in file: reading or direct modifications.
* * The original Command property should be replaced with incompatible changes later.

Also:

* +Id to ISolutionEvent
* Updated values of SolutionEventType & ModeType - Compatible changes for .vssbe
* ObjectExtension: fixed CloneBySerialization + added MD5Hash extension.
* Avoids actions by default in SolutionEvents. Only PreBuild.
* * Allows empty container for configuration: to support of compact .vssbe

vsSolutionBuildEvent/API/EventLevel.cs
@@ -103,7 +103,7 @@ namespace net.r_eg.vsSBE.API
         /// <param name="debug">Optional flag of debug mode</param>
         public void load(object dte2, bool debug = false)
         {
-            load(dte2, new Settings() { DebugMode = debug });
+            load(dte2, new API.Settings() { DebugMode = debug });
         }
 
         /// <summary>
@@ -129,7 +129,7 @@ namespace net.r_eg.vsSBE.API
         /// <param name="debug">Optional flag of debug mode</param>
         public void load(string sln, Dictionary<string, string> properties, bool debug = false)
         {
-            load(sln, properties, new Settings() { DebugMode = debug });
+            load(sln, properties, new API.Settings() { DebugMode = debug });
         }
 
         /// <summary>
@@ -375,13 +375,14 @@ namespace net.r_eg.vsSBE.API
             try {
                 Config._.load(Environment.SolutionPath, Environment.SolutionFileName);
                 Config._.updateActivation(Bootloader);
+                UserConfig._.load(Environment.SolutionPath, Environment.SolutionFileName);
 
                 UI.Plain.State.print(Config._.Data);
 #if DEBUG
-                Log.Warn("Used the [Debug version]");
+                Log.Warn("Used [Debug version]");
 #else
                 if(vsSBE.Version.branchName.ToLower() != "releases") {
-                    Log.Warn("Used the [Unofficial release]");
+                    Log.Warn("Used [Unofficial release]");
                 }
 #endif
 
@@ -455,7 +456,7 @@ namespace net.r_eg.vsSBE.API
         {
             (new Logger.Initializer()).configure();
 
-            vsSBE.Settings.debugMode = cfg.DebugMode;
+            vsSBE.Settings._.DebugMode = cfg.DebugMode;
             // ...
         }
 

vsSolutionBuildEvent/Settings.cs
@@ -17,73 +17,175 @@
 
 using System;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using net.r_eg.vsSBE.Exceptions;
+using net.r_eg.vsSBE.Configuration;
+using IUserData = net.r_eg.vsSBE.Configuration.User.IData;
 
 namespace net.r_eg.vsSBE
 {
-    /// <summary>
-    /// TODO:
-    /// </summary>
-    public static class Settings
+    internal sealed class Settings: IAppSettings
     {
         /// <summary>
-        /// Name of vsSBE log item in OutputWindowPane
+        /// Name of item in VS OutputWindow
         /// </summary>
-        public const string OWP_ITEM_VSSBE = "Solution Build-Events";
+        public const string OWP_ITEM_VSSBE = "vsSolutionBuildEvent";
 
         /// <summary>
-        /// Debug mode for this application
+        /// Name of application.
         /// </summary>
-        public static bool debugMode = false;
+        public const string APP_NAME = "vsSolutionBuildEvent";
 
         /// <summary>
-        /// Ignores all actions if value as true
-        /// Support of cycle control, e.g.: PRE -> POST [recursive DTE: PRE -> POST] -> etc.
+        /// Short name of application.
         /// </summary>
-        public static volatile bool silentModeActions = false;
+        public const string APP_NAME_SHORT = "vsSBE";
+        
+        /// <summary>
+        /// When DebugMode is updated.
+        /// Useful for clients, for example with IEntryPointClient.
+        /// </summary>
+        public event EventHandler<DataArgs<bool>> DebugModeUpdated = delegate(object sender, DataArgs<bool> e) { };
 
         /// <summary>
-        /// Path to this library
+        /// Debug mode for application.
         /// </summary>
-        public static string LibPath
+        public bool DebugMode
+        {
+            get {
+                return debugMode;
+            }
+            set {
+                debugMode = value;
+                DebugModeUpdated(this, new DataArgs<bool>() { Data = debugMode });
+            }
+        }
+        private bool debugMode = false;
+
+        /// <summary>
+        /// Ignores all actions if value set as true.
+        /// To support of cycle control, e.g.: PRE -> POST [recursive DTE: PRE -> POST] -> etc.
+        /// </summary>
+        public bool IgnoreActions
+        {
+            get { return ignoreActions; }
+            set { ignoreActions = value; }
+        }
+        private volatile bool ignoreActions = false;
+
+        /// <summary>
+        /// Full path to library.
+        /// </summary>
+        public string LibPath
         {
             get
             {
-                if(String.IsNullOrWhiteSpace(_libPath)) {
-                    _libPath = formatPath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+                if(String.IsNullOrWhiteSpace(libPath)) {
+                    libPath = formatPath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
                 }
-                return _libPath;
+                return libPath;
             }
         }
-        private static string _libPath;
+        private string libPath;
 
         /// <summary>
-        /// Root path for all operations
+        /// Working path for library.
         /// </summary>
-        public static string WorkingPath
+        public string WorkPath
         {
             get
             {
-                if(String.IsNullOrWhiteSpace(_workingPath)) {
-                    throw new SBEException("WorkingPath is empty or null");
+                if(String.IsNullOrWhiteSpace(workPath)) {
+                    workPath = "/";
+                    Log.Trace("WorkPath is empty or null, use '{0}' by default.", workPath);
+                    //throw new SBEException("WorkPath is empty or null");
                 }
-                return _workingPath;
+                return workPath;
             }
         }
-        private static string _workingPath;
-
+        private string workPath;
 
         /// <summary>
-        /// Sets new root path.
+        /// Main configuration data.
         /// </summary>
-        /// <param name="path"></param>
-        public static void setWorkingPath(string path)
+        public ISolutionEvents Config
         {
-            _workingPath = formatPath(path);
+            get { return net.r_eg.vsSBE.Config._.Data; }
         }
 
+        /// <summary>
+        /// User configuration data.
+        /// </summary>
+        public IUserData UserConfig
+        {
+            get { return net.r_eg.vsSBE.UserConfig._.Data; }
+        }
+
+        /// <summary>
+        /// Global configuration data.
+        /// </summary>
+        public IUserData GlobalConfig
+        {
+            get { return net.r_eg.vsSBE.GlobalConfig._.Data; }
+        }
+
+        /// <summary>
+        /// Updates working path for library.
+        /// </summary>
+        /// <param name="path">New path.</param>
+        public void setWorkPath(string path)
+        {
+            workPath = formatPath(path);
+        }
+
+        /// <summary>
+        /// Static alias. Main configuration data.
+        /// </summary>
+        public static ISolutionEvents Cfg
+        {
+            get { return _.Config; }
+        }
+
+        /// <summary>
+        /// Static alias. User configuration data.
+        /// </summary>
+        public static IUserData CfgUser
+        {
+            get { return _.UserConfig; }
+        }
+
+        /// <summary>
+        /// Static alias. Global configuration data.
+        /// </summary>
+        public static IUserData CfgGlobal
+        {
+            get { return _.GlobalConfig; }
+        }
+
+        /// <summary>
+        /// Static alias. Working path for library.
+        /// </summary>
+        public static string WPath
+        {
+            get { return _.WorkPath; }
+        }
+
+        /// <summary>
+        /// Static alias. Full path to library.
+        /// </summary>
+        public static string LPath
+        {
+            get { return _.libPath; }
+        }
+
+        /// <summary>
+        /// Thread-safe getting instance from Settings.
+        /// </summary>
+        public static Settings _
+        {
+            get { return _lazy.Value; }
+        }
+        private static readonly Lazy<Settings> _lazy = new Lazy<Settings>(() => new Settings());
+        
         /// <param name="path"></param>
         /// <returns></returns>
         private static string formatPath(string path)
@@ -97,5 +199,7 @@ namespace net.r_eg.vsSBE
             }
             return path;
         }
+
+        private Settings() { }
     }
 }

vsSolutionBuildEvent/Log.cs
@@ -216,7 +216,7 @@ namespace net.r_eg.vsSBE
             LogLevel oLevel = LogLevel.FromString(level);
 
 #if !DEBUG
-            if(oLevel < LogLevel.Info && !Settings.debugMode) {
+            if(oLevel < LogLevel.Info && !Settings._.DebugMode) {
                 return;
             }
 #endif

commit 59b48d935af3b2c66ab64e60fe021054d697dccb

Fixed bug of client.vssbe.dll for Isolated Environments.
"Client library: problem with initialization 'System.NullReferenceException: Object reference not set to an instance of an object."

+ Added raw CoreCommand - "DebugMode" (When DebugMode is updated) /However, I think, some event with common settings in IEntryPointCore is more useful.... later anyway -_-

also:

* Fixed LPath - broken from 26dba54
* Fixed frozen window in ClientDemo - tested on CIM & IDE

ClientDemo/Build.cs
@@ -34,7 +34,7 @@ namespace ClientDemo
         /// <param name="data">Raw data of building process</param>
         public void onBuildRaw(string data)
         {
-            Log._.info("Entering onBuildRaw(string data)");
+            Log._.info("Entering onBuildRaw(string data): '{0}'", (data.Length > 40)? data.Substring(0, 40) + "..." : data);
         }
 
         /// <summary>

ClientDemo/Log.cs
@@ -25,6 +25,7 @@
 using System;
 using System.Diagnostics;
 using System.Globalization;
+using System.Threading.Tasks;
 
 namespace ClientDemo
 {
@@ -45,9 +46,13 @@ namespace ClientDemo
         /// </summary>
         public void show()
         {
-            if(status != null) {
-                status.show();
+            if(status == null) {
+                return;
             }
+            
+            Task.Factory.StartNew(() => {
+                status.show();
+            });
         }
 
         /// <summary>

ClientDemo/StatusFrm.cs
@@ -11,7 +11,7 @@ namespace ClientDemo
         /// <param name="message"></param>
         public void report(string message)
         {
-            richTextBoxMain.Text += message;
+            push(richTextBoxMain, message);
         }
 
         /// <summary>
@@ -19,12 +19,26 @@ namespace ClientDemo
         /// </summary>
         public void show()
         {
-            Show();
+            ShowDialog();
         }
 
         public StatusFrm()
         {
             InitializeComponent();
+        }
+
+        protected void push(RichTextBox box, string message)
+        {
+            // box.InvokeRequired may does not check properly
+            try {
+                box.Text += message;
+            }
+            catch
+            {
+                box.Invoke((MethodInvoker)delegate {
+                    box.Text += message;
+                });
+            }
         }
 
         private void btnAPI_Click(object sender, System.EventArgs e)

vsSolutionBuildEvent/API/EventLevel.cs
@@ -25,6 +25,7 @@ using net.r_eg.vsSBE.Bridge.CoreCommand;
 using net.r_eg.vsSBE.Clients;
 using net.r_eg.vsSBE.SBEScripts;
 using net.r_eg.vsSBE.Scripts;
+using AppSettings = net.r_eg.vsSBE.Settings;
 
 namespace net.r_eg.vsSBE.API
 {
@@ -456,7 +457,11 @@ namespace net.r_eg.vsSBE.API
         {
             (new Logger.Initializer()).configure();
 
-            vsSBE.Settings._.DebugMode = cfg.DebugMode;
+            // TODO: event with common settings for IEntryPointCore
+            AppSettings._.DebugModeUpdated -= onDebugModeUpdated;
+            AppSettings._.DebugModeUpdated += onDebugModeUpdated;
+
+            AppSettings._.DebugMode = cfg.DebugMode;
             // ...
         }
 
@@ -485,6 +490,20 @@ namespace net.r_eg.vsSBE.API
                 cmdEvents.BeforeExecute -= _cmdBeforeExecute;
                 cmdEvents.AfterExecute  -= _cmdAfterExecute;
             }
+        }
+
+        /// <summary>
+        /// TODO: event with common settings for IEntryPointCore
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void onDebugModeUpdated(object sender, DataArgs<bool> e)
+        {
+            fire(new CoreCommandArgs()
+            { 
+                Type = CoreCommandType.RawCommand,
+                Args = new object[] { new KeyValuePair<string, bool>("DebugMode", e.Data) }
+            });
         }
 
         /// <summary>

vsSolutionBuildEvent/Clients/ClientLibrary.cs
@@ -131,6 +131,7 @@ namespace net.r_eg.vsSBE.Clients
         /// <returns>true value if library exists and successfully loaded</returns>
         public bool tryLoad(IEntryPointCore core, string sln, Dictionary<string, string> properties)
         {
+            this.core       = core;
             solutionFile    = sln;
             this.properties = properties;
 

vsSolutionBuildEvent/Settings.cs
@@ -174,7 +174,7 @@ namespace net.r_eg.vsSBE
         /// </summary>
         public static string LPath
         {
-            get { return _.libPath; }
+            get { return _.LibPath; }
         }
 
         /// <summary>