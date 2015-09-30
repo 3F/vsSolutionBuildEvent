beb7ef7f62ea0b195553471dc8fd98f6f9344350
reg <entry.reg@gmail.com>
2015-09-30 13:30:10 +0300

    Updated vsSBE.CI.MSBuild to v1.5.1

--- build_[CI_Release].bat
+++ build_[CI_Release].bat
@@ -1,2 +1,2 @@
 .nuget\nuget restore vsSolutionBuildEvent_net40.sln 
-"C:\Program Files (x86)\MSBuild\12.0\bin\msbuild.exe" "vsSolutionBuildEvent_net40.sln" /verbosity:detailed  /l:"packages\vsSBE.CI.MSBuild.1.5.0\bin\CI.MSBuild.dll" /m:12 /t:Rebuild /p:Configuration=CI_Release /p:Platform="Any CPU"
+"C:\Program Files (x86)\MSBuild\12.0\bin\msbuild.exe" "vsSolutionBuildEvent_net40.sln" /verbosity:detailed  /l:"packages\vsSBE.CI.MSBuild.1.5.1\bin\CI.MSBuild.dll" /m:12 /t:Rebuild /p:Configuration=CI_Release /p:Platform="Any CPU"
