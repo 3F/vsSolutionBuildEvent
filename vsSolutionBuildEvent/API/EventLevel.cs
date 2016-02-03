3F/vsSolutionBuildEvent

commit ca44449da139333d11d31b4612d67d944fb3b4c3

New Event types: 'Sln-Opened' & 'Sln-Closed'
http://vssbe.r-eg.net/doc/Events/SlnOpened/

also:

* build:
    * fixed DefineConstants: NET_40 & DEBUG;TRACE
    * version of msbuild tool now is v14 by default

vsSolutionBuildEvent/API/EventLevel.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (c) 2013-2015  Denis Kuzmin (reg) <entry.reg@gmail.com>
+ * Copyright (c) 2013-2016  Denis Kuzmin (reg) <entry.reg@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -60,6 +60,11 @@ namespace net.r_eg.vsSBE.API
         /// Provides command events for automation clients
         /// </summary>
         protected EnvDTE.CommandEvents cmdEvents;
+
+        /// <summary>
+        /// The low priority level of the solution events.
+        /// </summary>
+        protected EnvDTE.SolutionEvents slnEvents;
 
         /// <summary>
         /// Access to client library
@@ -401,7 +406,12 @@ namespace net.r_eg.vsSBE.API
             UI.Plain.State.print(config.Data);
 
             OpenedSolution(this, new EventArgs());
-            clientLib.Event.solutionOpened(pUnkReserved, fNewSolution);
+
+            // delay calling
+            lock(_lock) {
+                slnEvents.Opened -= slnOpenedLowPriority;
+                slnEvents.Opened += slnOpenedLowPriority;
+            }
             return Codes.Success;
         }
 
@@ -412,12 +422,21 @@ namespace net.r_eg.vsSBE.API
         /// <returns>If the method succeeds, it returns Codes.Success. If it fails, it returns an error code.</returns>
         public int solutionClosed(object pUnkReserved)
         {
-            clientLib.Event.solutionClosed(pUnkReserved);
+            int ret;
+            try {
+                ret = Action.bindSlnClosed();
+                clientLib.Event.solutionClosed(pUnkReserved);
+            }
+            catch(Exception ex) {
+                Log.Error("Failed Solution.SlnClosed-binding: `{0}`", ex.Message);
+                ret = Codes.Failed;
+            }
+
             ClosedSolution(this, new EventArgs());
 
             ConfigManager.Config.unload();
             ConfigManager.UserConfig.unload();
-            return Codes.Success;
+            return ret;
         }
 
         /// <summary>
@@ -454,7 +473,8 @@ namespace net.r_eg.vsSBE.API
                 }
 #endif
 
-            Environment.CoreCmdSender = this;
+            slnEvents                   = Environment.Events.SolutionEvents;
+            Environment.CoreCmdSender   = this;
             attachCommandEvents();
 
             this.Bootloader = new Bootloader(Environment, uvariable);
@@ -513,9 +533,29 @@ namespace net.r_eg.vsSBE.API
             if(cmdEvents == null) {
                 return;
             }
+
             lock(_lock) {
                 cmdEvents.BeforeExecute -= _cmdBeforeExecute;
                 cmdEvents.AfterExecute  -= _cmdAfterExecute;
+            }
+        }
+
+        /// <summary>
+        /// The low priority handler for work only when all projects are opened.
+        /// Mainly we work with priority ( http://stackoverflow.com/q/27018762 ) so this should be handled after the all 'OnAfterOpenProject' etc. as non-priority 'IVsSolutionEvents.OnAfterOpenSolution'
+        /// </summary>
+        private void slnOpenedLowPriority()
+        {
+            lock(_lock)
+            {
+                slnEvents.Opened -= slnOpenedLowPriority;
+                try {
+                    Action.bindSlnOpened();
+                    clientLib.Event.solutionOpened(new object(), 0); //TODO: use from solutionOpened(object pUnkReserved, int fNewSolution)
+                }
+                catch(Exception ex) {
+                    Log.Error("Failed Solution.SlnOpened-binding: `{0}`", ex.Message);
+                }
             }
         }
 

.vssbe
@@ -341,7 +341,7 @@
       "Caption": "Definitions for POST",
       "SupportMSBuild": true,
       "SupportSBEScripts": true,
-      "IgnoreIfBuildFailed": true,
+      "IgnoreIfBuildFailed": false,
       "BuildType": "Common",
       "Confirmation": false,
       "ToConfiguration": [],

build/[Release][net40]_v12.bat
@@ -1,12 +1,0 @@
-call "%~dp0_config"
-
-REM # Configuration
-set cfgname=Release
-
-REM # Solution file
-set sln=vsSolutionBuildEvent_net40.sln
-
-REM # Version of MSBuild tool
-set _msbuild=12.0
-
-call "%~dp0_build"

build/[Release][net40]_v14.bat
@@ -1,12 +1,0 @@
-call "%~dp0_config"
-
-REM # Configuration
-set cfgname=Release
-
-REM # Solution file
-set sln=vsSolutionBuildEvent_net40.sln
-
-REM # Version of MSBuild tool
-set _msbuild=14.0
-
-call "%~dp0_build"

build/[Release][net45]_v12.bat
@@ -1,12 +1,0 @@
-call "%~dp0_config"
-
-REM # Configuration
-set cfgname=Release
-
-REM # Solution file
-set sln=vsSolutionBuildEvent_2013.sln
-
-REM # Version of MSBuild tool
-set _msbuild=12.0
-
-call "%~dp0_build"

build/[Release][net45]_v14.bat
@@ -3,9 +3,6 @@ call "%~dp0_config"
 REM # Configuration
 set cfgname=Release
 
-REM # Solution file
-set sln=vsSolutionBuildEvent_2015.sln
-
 REM # Version of MSBuild tool
 set _msbuild=14.0
 

build/[Release_with_revision][net40]_v12.bat
@@ -1,12 +1,0 @@
-call "%~dp0_config"
-
-REM # Configuration
-set cfgname=Release_with_revision
-
-REM # Solution file
-set sln=vsSolutionBuildEvent_net40.sln
-
-REM # Version of MSBuild tool
-set _msbuild=12.0
-
-call "%~dp0_build"

build/[Release_with_revision][net45]_v12.bat
@@ -1,12 +1,0 @@
-call "%~dp0_config"
-
-REM # Configuration
-set cfgname=Release_with_revision
-
-REM # Solution file
-set sln=vsSolutionBuildEvent_2013.sln
-
-REM # Version of MSBuild tool
-set _msbuild=12.0
-
-call "%~dp0_build"

commit 02471f7062f64a040d02426d559d2f31939994bd

Fixed NullReferenceException when used IsolatedEnv (broken in ca44449) and problem with paths of HProcess (broken in 19d0217)

vsSolutionBuildEvent/API/EventLevel.cs
@@ -407,6 +407,10 @@ namespace net.r_eg.vsSBE.API
 
             OpenedSolution(this, new EventArgs());
 
+            if(slnEvents == null) {
+                return slnOpened(pUnkReserved, fNewSolution); // we can work in normal mode
+            }
+
             // delay calling
             lock(_lock) {
                 slnEvents.Opened -= slnOpenedLowPriority;
@@ -473,8 +477,11 @@ namespace net.r_eg.vsSBE.API
                 }
 #endif
 
-            slnEvents                   = Environment.Events.SolutionEvents;
-            Environment.CoreCmdSender   = this;
+            if(Environment.Events != null) {
+                slnEvents = Environment.Events.SolutionEvents;
+            }
+
+            Environment.CoreCmdSender = this;
             attachCommandEvents();
 
             this.Bootloader = new Bootloader(Environment, uvariable);
@@ -548,15 +555,24 @@ namespace net.r_eg.vsSBE.API
         {
             lock(_lock)
             {
-                slnEvents.Opened -= slnOpenedLowPriority;
-                try {
-                    Action.bindSlnOpened();
-                    clientLib.Event.solutionOpened(new object(), 0); //TODO: use from solutionOpened(object pUnkReserved, int fNewSolution)
+                if(slnEvents != null) {
+                    slnEvents.Opened -= slnOpenedLowPriority;
                 }
-                catch(Exception ex) {
-                    Log.Error("Failed Solution.SlnOpened-binding: `{0}`", ex.Message);
-                }
+                slnOpened(new object(), 0); //TODO: use from solutionOpened(object pUnkReserved, int fNewSolution)
             }
+        }
+
+        private int slnOpened(object pUnkReserved, int fNewSolution)
+        {
+            try {
+                int ret = Action.bindSlnOpened();
+                clientLib.Event.solutionOpened(pUnkReserved, fNewSolution);
+                return ret;
+            }
+            catch(Exception ex) {
+                Log.Error("Failed Solution.SlnOpened-binding: `{0}`", ex.Message);
+            }
+            return Codes.Failed;
         }
 
         /// <summary>

vsSolutionBuildEvent/Actions/Action.cs
@@ -27,8 +27,6 @@ namespace net.r_eg.vsSBE.Actions
         /// </summary>
         protected ICommand cmd;
 
-        protected HProcess hprocess;
-
         /// <summary>
         /// Process for specified event.
         /// </summary>
@@ -44,7 +42,9 @@ namespace net.r_eg.vsSBE.Actions
         public virtual void shell(ISolutionEvent evt, string cmd)
         {
             Log.Info("Prepared command: '{0}'", cmd);
-            hprocess.useShell(cmd, evt.Id, evt.Process.Waiting, evt.Process.Hidden, evt.Process.TimeLimit);
+
+            HProcess p = new HProcess(Settings.WPath);
+            p.useShell(cmd, evt.Id, evt.Process.Waiting, evt.Process.Hidden, evt.Process.TimeLimit);
         }
 
         /// <summary>
@@ -70,7 +70,6 @@ namespace net.r_eg.vsSBE.Actions
         public Action(ICommand cmd)
         {
             this.cmd = cmd;
-            hprocess = new HProcess(Settings.WPath);
         }
 
         protected string treatNewlineAs(string str, string data)

vsSolutionBuildEvent/Actions/HProcess.cs
@@ -51,7 +51,7 @@ namespace net.r_eg.vsSBE.Actions
             internal static extern int GetSystemDefaultLCID();
         }
 
-        internal class TStream
+        internal sealed class TStream
         {
             public string stdout;
             public string stderr;
@@ -321,7 +321,7 @@ namespace net.r_eg.vsSBE.Actions
 
         private static TStream streamContainer(Guid id)
         {
-            if(!stdstream.ContainsKey(id)) {
+            if(!stdstream.ContainsKey(id) || stdstream[id] == null) {
                 stdstream[id] = new TStream();
             }
             return stdstream[id];